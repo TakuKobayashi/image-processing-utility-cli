417e5f6e81ff052becce9036f7511730a3a32f84
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2025-05-12 21:16:49 +0900

    feat: convert to image file.

--- src/cli.ts
+++ src/cli.ts
@@ -2,6 +2,9 @@ import { program } from 'commander';
 import packageJson from '../package.json';
 import path from 'node:path';
 import fg from 'fast-glob';
+import sharp from 'sharp';
+import crypto from 'node:crypto';
+import fs from 'node:fs/promises';
 
 /**
  * Set global CLI configurations
@@ -16,10 +19,13 @@ program
   .option('-p, --root-path <path>', 'display just the first substring', './')
   .option('-f, --from-extension <fromExt>', 'separator character', 'jpg')
   .option('-t, --to-extension <toExt>', 'separator character', 'png')
-  .action((options) => {
-    console.log(options);
+  .action(async (options) => {
     const imageFilePathes = fg.sync([...options.rootPath.split(path.sep), '**', `*.${options.fromExtension}`].join('/'), { dot: true });
-    console.log(imageFilePathes);
+    for(const imageFilePath of imageFilePathes) {
+      const outputFilePath = imageFilePath.replace(path.extname(imageFilePath), `.${options.toExtension}`)
+      const converted = await sharp(imageFilePath).png().toFile(outputFilePath)
+      console.log(converted);
+    }
   });
 
 program.parse(process.argv);
