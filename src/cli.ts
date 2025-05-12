d29d152a091230c8e5eeffe9b8cda79e3ae010e6
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2025-05-12 20:55:32 +0900

    feat: search image files.

--- src/cli.ts
+++ src/cli.ts
@@ -1,5 +1,7 @@
-import { program, Command } from 'commander';
+import { program } from 'commander';
 import packageJson from '../package.json';
+import path from 'node:path';
+import fg from 'fast-glob';
 
 /**
  * Set global CLI configurations
@@ -11,11 +13,13 @@ program.version(packageJson.version, '-v, --version');
 program
   .command('convert')
   .description('Split a string into substrings and display as an array')
-  .option('-p, --root-path <path>', 'display just the first substring')
+  .option('-p, --root-path <path>', 'display just the first substring', './')
   .option('-f, --from-extension <fromExt>', 'separator character', 'jpg')
   .option('-t, --to-extension <toExt>', 'separator character', 'png')
   .action((options) => {
     console.log(options);
+    const imageFilePathes = fg.sync([...options.rootPath.split(path.sep), '**', `*.${options.fromExtension}`].join('/'), { dot: true });
+    console.log(imageFilePathes);
   });
 
 program.parse(process.argv);
