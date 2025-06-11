0efc3426c646b0497c255f8973e873b561145868
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2025-06-12 04:07:04 +0900

    feat: bulk-export-exif command

--- src/cli.ts
+++ src/cli.ts
@@ -4,8 +4,8 @@ import path from 'node:path';
 import fg from 'fast-glob';
 import sharp, { OutputInfo, FormatEnum, AvailableFormatInfo } from 'sharp';
 import fs from 'node:fs';
-import ExifReader from 'exifreader';
-import cliProgress, { SingleBar } from 'cli-progress';
+import ExifReader, { Tags } from 'exifreader';
+import cliProgress from 'cli-progress';
 
 /**
  * Set global CLI configurations
@@ -40,7 +40,6 @@ program
   .option('-f, --format <format>', 'convert to image format')
   .argument('<output>', 'output directory path')
   .action(async (output, options) => {
-    const imageFilePathes: string[] = [];
     const inputDirectoryPath = options.input;
     if (!inputDirectoryPath || !fs.existsSync(inputDirectoryPath)) {
       throw new Error('input directory not found');
@@ -55,13 +54,7 @@ program
       throw new Error('output path is not directory');
     }
 
-    const filePathes = fg.sync(
-      [...inputDirectoryPath.toString().split(path.sep), '**', `*.${inputFormat.toString().toLowerCase()}`].join('/'),
-      { dot: true },
-    );
-    for (const filePath of filePathes) {
-      imageFilePathes.push(filePath);
-    }
+    const imageFilePathes = loadImageFilePathes(inputDirectoryPath.toString(), inputFormat.toString());
     const convertImageFormat = options.format.toString().toLowerCase();
     const convertPromises: Promise<any>[] = [];
     const progressBar = new cliProgress.SingleBar(
@@ -98,15 +91,72 @@ program
     if (!inputFilePath || !fs.existsSync(inputFilePath)) {
       throw new Error('input file not found');
     }
-    const info = await ExifReader.load(inputFilePath);
-    const output = options.output;
-    if (output) {
-      const outputFilePath = path.resolve(output);
-      fs.writeFileSync(outputFilePath, JSON.stringify(info));
-    } else {
-      console.log(info);
-    }
+    await IOExifInfo(inputFilePath, options.output);
   });
+
+program
+  .command('bulk-export-exif')
+  .description('search and convert image files')
+  .option('-i, --input <path>', 'input directory path')
+  .option('--if, --input-format <format>', 'filter input file format')
+  .option('-o, --output <path>', 'output json directory filepath')
+  .action(async (options) => {
+    const inputDirectoryPath = options.input;
+    if (!inputDirectoryPath || !fs.existsSync(inputDirectoryPath)) {
+      throw new Error('input directory not found');
+    }
+    const inputFormat = options.inputFormat;
+    if (!inputFormat) {
+      throw new Error('input image format is none');
+    }
+    const output = options.output;
+    const outputDirectoryPath = path.resolve(output);
+    const outputStat = fs.lstatSync(outputDirectoryPath);
+    if (!outputStat.isDirectory()) {
+      throw new Error('output path is not directory');
+    }
+    const imageFilePathes = loadImageFilePathes(inputDirectoryPath.toString(), inputFormat.toString());
+    const progressBar = new cliProgress.SingleBar(
+      {
+        format: '{bar} | {value}/{total} | {inputFileName} => {outputFileName}',
+      },
+      cliProgress.Presets.shades_classic,
+    );
+    progressBar.start(imageFilePathes.length, 0);
+    const exifPromises: Promise<any>[] = [];
+    for (const imageFilePath of imageFilePathes) {
+      const imageFileName = path.basename(imageFilePath.toString().split(path.sep).join('/'));
+      const outputImageName = imageFileName.replace(path.extname(imageFileName), `.json`);
+      const outputImagePath = path.resolve(outputDirectoryPath, outputImageName);
+      exifPromises.push(
+        IOExifInfo(imageFilePath, outputImagePath).then((result) => {
+          progressBar.increment(1, {
+            inputFileName: imageFilePath,
+            outputFileName: outputImagePath,
+          });
+        }),
+      );
+    }
+    await Promise.all(exifPromises);
+  });
+
+async function IOExifInfo(inputFilePath: string, writeJsonFilePath: string | null = null): Promise<Tags> {
+  const info = await ExifReader.load(inputFilePath);
+  if (writeJsonFilePath) {
+    const outputFilePath = path.resolve(writeJsonFilePath);
+    fs.writeFileSync(outputFilePath, JSON.stringify(info));
+  }
+  return info;
+}
+
+function loadImageFilePathes(inputDirectoryPath: string, inputFormat: string): string[] {
+  const imageFilePathes: string[] = [];
+  const filePathes = fg.sync([...inputDirectoryPath.split(path.sep), '**', `*.${inputFormat.toLowerCase()}`].join('/'), { dot: true });
+  for (const filePath of filePathes) {
+    imageFilePathes.push(filePath);
+  }
+  return imageFilePathes;
+}
 
 async function convertImage(
   imageFilePath: string,
